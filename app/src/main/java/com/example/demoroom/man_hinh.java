d21a32cc962735efcac498c77438d2432b4acb4c
hai_dangnhap <https://github.com/vanhai0k/Anh-H-i.git>
2022-11-10 22:05:07 -0700

    hải

---
+++ app/src/main/java/com/example/demoroom/man_hinh.java
@@ -0,0 +1,14 @@
+package com.example.demoroom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class man_hinh extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_man_hinh);
+    }
+}
